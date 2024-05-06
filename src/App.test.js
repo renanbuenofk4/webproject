d5ff2966e61002cfb4d03b3b8981bfd3c0eead42
renanbuenofk4 <166943934+renanbuenofk4@users.noreply.github.com>
2024-05-06 14:15:24 -0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders custom text', () => {
   render(<App />);
-  const customTextElement = screen.getByText(/Test-job-OK/i);
+  const customTextElement = screen.getByText(test-job);
   expect(customTextElement).toBeInTheDocument();
 });
